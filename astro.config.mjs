bebeb032020a9637dc08c0617dfde021e32fa1cd
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-06-26 15:53:40 -0700

    Reconfigure base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://topography.blog',
-  base: '/as-topography'
+  site: 'https://topography.blog'
 });
