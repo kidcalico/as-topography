8102b30bde0a894430c8965cf3040bd4b16bd1cf
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-06-26 15:36:44 -0700

    Configure github to deploy pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kidcalico.github.io',
+  site: 'https://topography.blog',
   base: '/as-topography'
 });
