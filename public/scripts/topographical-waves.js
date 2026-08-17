9345bc9a4caf590042d2b7768321165301fb72ed
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-08-17 16:05:12 -0700

    Add light mode to p5 sketch

--- public/scripts/topographical-waves.js
+++ public/scripts/topographical-waves.js
@@ -10,13 +10,24 @@ function setup() {
   canvas.parent(p5sketch);
 } 
 
+function getColors() {
+  const isLight = document.documentElement.classList.contains('light');
+  return {
+    background: isLight ? '#fef9df' : '#111',
+    stroke: isLight ? '#8d6300' : '#ffd67e',
+  }
+}
+
 function draw() {
-  background(color('#111'));
+  const colors = getColors();
+  background(colors.background);
+  // background(color('#111'));
   translate(0, 800);
   rotate(-25);
   for (let offset = 0; offset >= -300; offset -= 20) {
     beginShape();
-    stroke(color('#ffd67e'));
+    stroke(colors.stroke);
+    // stroke(color('#ffd67e'));
     strokeWeight(2);
     noFill();
     let xoff = start
